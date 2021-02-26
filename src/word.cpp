961e0b8768110952698d2956e134b5aadd6656af
mille5a9 <andrewm192@outlook.com>
2021-02-26 07:07:06 +0000

    expanded word struct

--- src/word.cpp
+++ src/word.cpp
@@ -1,11 +1,26 @@
 #include "word.h"
+#include <ctype.h>
+
+Word::Word(const char *name, int lineNum, 
+    int colNum, int type = 340, Record scopeToken = NULL) {
+        this->tokenString = name;
+        this->tokenType = type;
+        this->scope = scopeToken;
+        this->line = lineNum;
+        this->col = colNum;
+}
 
 // determines whether or not the character is a letter, while capitalizing it
 bool Word::isLetter(char &in) {
-
+    if (isalpha(in)) {
+        in = toupper(in);
+        return true;
+    }
+    else return false;
 }
 
 // determines whether or not the char is a digit
 bool Word::isDigit(char &in) {
-
+    if (isdigit(in)) return true;
+    else return false;
 }
--- src/word.h
+++ src/word.h
@@ -1,8 +1,16 @@
 #ifndef WORD_H
 #define WORD_H
 
-struct Word {
+#include "symboltable.h"
 
+struct Word {
+    Word(const char *name,int lineNum, int colNum,  int type = 340, Record scopeToken = NULL);
+    const char *tokenString;
+    int tokenType = 340, line, col;
+    Record scope;
+    char stringValue[256];
+    int intValue = 0; // also used for bool
+    float floatValue = 0.0;
 
     static bool isLetter(char &in);
     static bool isDigit(char &in);
