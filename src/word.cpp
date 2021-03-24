a5c1bbc981f98b2424e9a452924df8efe66cf6e4
mille5a9 <andrewm192@outlook.com>
2021-03-24 03:20:35 +0000

    implement word factory methods

--- src/word.cpp
+++ src/word.cpp
@@ -2,7 +2,6 @@
 #include "word.h"
 #include "symboltable.h"
 #include <ctype.h>
-#include <string>
 #include <algorithm>
 
 Word::Word(std::string name, int lineNum, int colNum, int type) {
@@ -12,26 +11,32 @@ Word::Word(std::string name, int lineNum, int colNum, int type) {
     this->col = colNum;
 }
 
-DigitWord::DigitWord(std::string name, int lineNum, int colNum,  int type)  :
-    Word(name, lineNum, colNum, type) {
-
-    // store value as int, get rid of underscores first
-    if (type == T_INTEGER) {
-        name.erase(std::remove(name.begin(), name.end(), '_'), name.end());
-        this->intValue = std::stoi(name);
-    }
-
-    // store value as float, get rid of underscores first
-    else if (type == T_FLOAT) {
-        name.erase(std::remove(name.begin(), name.end(), '_'), name.end());
-        this->floatValue = std::stof(name);
-    }
+Word WordFactory::createGenericWord(std::string name, int lineNum, int colNum, int type) {
+    return Word(name, lineNum, colNum, type);
 }
 
-StringWord::StringWord(std::string name, int lineNum, int colNum,  int type)  :
-    Word(name, lineNum, colNum, type) {
+// parses the name string into intvalue or float value during word creation
+Word WordFactory::createDigitWord(std::string name, int lineNum, int colNum, int type) {
+    Word output = Word(name, lineNum, colNum, type);
+    name.erase(std::remove(name.begin(), name.end(), '_'), name.end());
+    if (type == T_ILITERAL) output.intValue = std::stoi(name);
+    if (type == T_FLITERAL) output.floatValue = std::stof(name);
+    return output;
+}
+
+// parses the name string into stringvalue during word creation
+Word WordFactory::createStringWord(std::string name, int lineNum, int colNum, int type) {
+    Word output = Word(name, lineNum, colNum, type);
 
     // trim the quotes off the string and store the raw string data
     name = name.substr(1, name.size() - 2);
-    this->strValue = name;
+    output.strValue = name;
+    return output;
+}
+
+// makes a word, possibly sets the flag to denote a procedure ID
+Word WordFactory::createIdWord(std::string name, int lineNum, int colNum, int type, bool isProc) {
+    Word output = Word(name, lineNum, colNum, type);
+    output.isProcIdentifier = isProc;
+    return output;
 }
